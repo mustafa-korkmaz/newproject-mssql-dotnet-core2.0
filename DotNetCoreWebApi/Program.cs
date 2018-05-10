19d53c4b5992567bdca2236ccbe6fa9671b00730
mustafa-korkmaz <m.korkmaz@outlook.com>
2018-05-10 23:26:48 +0300

    IIS publish configurations added.

--- DotNetCoreWebApi/Program.cs
+++ DotNetCoreWebApi/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace WebApi
@@ -12,6 +13,9 @@ namespace WebApi
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
     }
