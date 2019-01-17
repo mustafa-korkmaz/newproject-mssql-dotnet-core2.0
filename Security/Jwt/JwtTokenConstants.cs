6bebf0d1ac549ecec4d6db618dcac5108ae5d69b
mustafa-korkmaz <m.korkmaz@outlook.com>
2019-01-17 14:49:19 +0300

    nuget packages updates.

--- Security/Jwt/JwtTokenConstants.cs
+++ Security/Jwt/JwtTokenConstants.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.IdentityModel.Tokens;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Security.Jwt
 {
