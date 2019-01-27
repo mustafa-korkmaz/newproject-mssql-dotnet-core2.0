cd96f3d782fc58135df890a587cae5881f3cf31a
mustafa-korkmaz <m.korkmaz@outlook.com>
2019-01-27 16:31:50 +0300

    added repository instance creation via interface.

--- Dal/UnitOfWork.cs
+++ Dal/UnitOfWork.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using Microsoft.EntityFrameworkCore.Storage;
 using Dal.Models;
 using System.Threading.Tasks;
@@ -70,7 +72,41 @@ namespace Dal
 
             if (!_repositories.ContainsKey(type))
             {
-                var repositoryType = typeof(TRepository);
+                var repositoryInterfaceType = typeof(TRepository);
+
+                var assignedTypesToRepositoryInterface = Assembly.GetExecutingAssembly().GetTypes().Where(t => repositoryInterfaceType.IsAssignableFrom(t)); //all types of your plugin
+
+                var repositoryType = assignedTypesToRepositoryInterface.First(p => p.Name[0] != 'I'); //filter interfaces, select only first implemented class
+
+                var repositoryInstance = Activator.CreateInstance(repositoryType, _context);
+                _repositories.Add(type, repositoryInstance);
+            }
+
+            return (TRepository)_repositories[type];
+        }
+
+        /// <summary>
+        /// Use only for entity object free repositories
+        /// </summary>
+        /// <typeparam name="TRepository"></typeparam>
+        /// <returns></returns>
+        public TRepository Repository<TRepository>()
+        {
+            if (_repositories == null)
+            {
+                _repositories = new Dictionary<string, object>();
+            }
+
+            var type = "EntityFree_" + typeof(TRepository).Name;
+
+            if (!_repositories.ContainsKey(type))
+            {
+                var repositoryInterfaceType = typeof(TRepository);
+
+                var assignedTypesToRepositoryInterface = Assembly.GetExecutingAssembly().GetTypes().Where(t => repositoryInterfaceType.IsAssignableFrom(t)); //all types of your plugin
+
+                var repositoryType = assignedTypesToRepositoryInterface.First(p => p.Name[0] != 'I'); //filter interfaces, select only first implemented class
+
                 var repositoryInstance = Activator.CreateInstance(repositoryType, _context);
                 _repositories.Add(type, repositoryInstance);
             }
